c1bafa181a9f77b5f77e6764a8d0a0e517e3af96
Polluix <lipe311973@gmail.com>
2022-09-22 12:04:39 -0500

    MaterialApp and Scaffold

--- HomePage.dart
+++ HomePage.dart
@@ -23,3 +23,22 @@ class HomePage extends StatelessWidget {
     );
   }
 }
+/* // Scaffold
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () => print('Flutterando'),
+      ),
+      appBar: AppBar(
+        title: const Text('Home'),
+      ),
+      body: const Center(
+        child: Text('Flutterando', textDirection: TextDirection.rtl),
+      ),
+    );
+  }
+}
+*/
