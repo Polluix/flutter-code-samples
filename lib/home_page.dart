ae52aefca437c465fda552d6e8d2e49d25271018
Polluix <lipe311973@gmail.com>
2022-09-24 16:24:56 -0300

    StatefulWidget implementation

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hello_world/home_controller.dart';
-
+/*
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -10,6 +10,7 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
+        // !Deprecated method, do not use!!!!
         onPressed: () => controller.increment(),
       ),
       appBar: AppBar(
@@ -17,6 +18,37 @@ class HomePage extends StatelessWidget {
       ),
       body: Center(
         child: Text('Flutterando ${controller.value}'),
+      ),
+    );
+  }
+}
+*/
+
+// *StatefulWidget => easier way to control the state compared to StatelessWidget
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var counter = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () => setState(() {
+          counter++;
+        }),
+      ),
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      body: Center(
+        child: Text('Flutterando $counter'),
       ),
     );
   }
