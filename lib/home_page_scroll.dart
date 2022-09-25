902d4ffaa212f4bb0ced00c8505cc2d7dea17f20
Polluix <lipe311973@gmail.com>
2022-09-25 12:04:58 -0300

    Home Page with column and scroll

---
+++ lib/home_page_scroll.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+// *StatefulWidget => easier way to control the state compared to StatelessWidget
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var counter = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () => setState(() {
+          counter++;
+        }),
+      ),
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      body: SizedBox(
+          width: double.infinity,
+          height: double.infinity,
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                for (var i = 0; i < 15; i++)
+                  Container(
+                    width: double.infinity,
+                    height: 80,
+                    margin: const EdgeInsets.all(12),
+                    color: Colors.red,
+                  )
+              ],
+            ),
+          )),
+    );
+  }
+}
