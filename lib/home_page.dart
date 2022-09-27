e548688173d1b90122f2391cdfb5ebe8e6317fa0
Polluix <lipe311973@gmail.com>
2022-09-27 12:29:51 -0300

    ListView.builder and Container

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -32,7 +32,7 @@ class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
 }
-
+/*
 class _HomePageState extends State<HomePage> {
   var counter = 0;
   @override
@@ -47,8 +47,84 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: Text('Home'),
       ),
-      body: Center(
-        child: Text('Flutterando $counter'),
+      body: SizedBox(
+          width: double.infinity,
+          height: double.infinity,
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                for (var i = 0; i < 15; i++)
+                  Container(
+                    width: double.infinity,
+                    height: 80,
+                    margin: const EdgeInsets.all(12),
+                    color: Colors.red,
+                  )
+              ],
+            ),
+          )),
+    );
+  }
+}
+*/
+
+class _HomePageState extends State<HomePage> {
+  var counter = 0;
+  @override
+  Widget build(BuildContext context) {
+    final names = [
+      'nome1',
+      'nome2',
+      'nome3',
+      'nome4',
+      'nome5',
+      'nome6',
+      'nome7',
+      'nome8',
+      'nome9',
+      'nome10',
+      'nome11',
+      'nome12',
+      'nome13',
+      'nome14',
+      'nome15'
+    ];
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () => setState(() {
+          counter++;
+        }),
+      ),
+      appBar: AppBar(
+        title: const Text('Home'),
+      ),
+      body: SizedBox(
+        width: double.infinity,
+        height: double.infinity,
+        child: ListView.builder(
+          itemCount: names.length,
+          itemBuilder: (context, index) {
+            return Container(
+              width: double.infinity,
+              height: 80,
+              margin: const EdgeInsets.all(12),
+              color: Colors.red,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ListTile(
+                    title: Text(
+                      textAlign: TextAlign.center,
+                      names[index],
+                    ),
+                  )
+                ],
+              ),
+            );
+          },
+        ),
       ),
     );
   }
